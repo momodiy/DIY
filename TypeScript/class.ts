fd7c87cfb2779ba68ce83d3d8a12738c3cb37723
steven <qcjy16@126.com>
2018-06-19 23:24:26 +0800

    :construction: add ts class learning code and some comments

--- TypeScript/class.ts
+++ TypeScript/class.ts
@@ -101,8 +101,47 @@ console.log(grid2.calculateDistanceFromOrigin({x: 10, y: 10}));
 // console.log(grid1.origin);   Error
 console.log(grid1.common);
 
+/*
+* abstract
+* 抽象类 抽象方法
+* 无法对一个抽象类创建实例
+* 定义的抽象方法必须在子类中实现
+* */
 
+abstract class Department {
 
+    constructor(public name: string) {
+        console.log(999);
+    }
+
+    printName(): void {
+        console.log('Department name: ' + this.name);
+    }
+
+    abstract printMeeting(): void; // 必须在子类中实现
+}
+
+class AccountingDepartment extends Department {
+
+    constructor() {
+        super('Accounting and Auditing'); // 在派生类的构造函数中必须调用 super()
+    }
+
+    printMeeting(): void {
+        console.log('The Accounting Department meets each Monday at 10am.');
+    }
+
+    generateReports(): void {
+        console.log('Generating accounting reports...');
+    }
+}
+
+let department: Department; // 允许创建一个对抽象类型的引用
+// department = new Department(); // 错误: 不能创建一个抽象类的实例
+department = new AccountingDepartment(); // 允许对一个抽象子类进行实例化和赋值
+department.printName();
+department.printMeeting();
+// department.generateReports(); // 错误: 方法在声明的抽象类中不存在
 
 //将文件声明为 module ，其中的变量回作为当前作用域下的局部变量
 export {};
