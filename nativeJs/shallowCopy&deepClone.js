71ae47d163b88cd395832d5df65abf0f6eac5825
StevenLee <qcjy16@126.com>
2021-04-11 23:30:17 +0800

    :sparkles: shallowCopy & deepClone

---
+++ nativeJs/shallowCopy&deepClone.js
@@ -0,0 +1,36 @@
+/*
+* Created by Steven Lee
+* Date: 2021/4/10
+* Time: 19:21
+* Project: DIY
+* */
+
+function shallowCopy(obj) {
+  if (typeof obj !== 'object') return
+
+  let newObj = obj instanceof Array ? [] : {}
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      newObj[key] = obj[key]
+    }
+  }
+  return newObj
+}
+
+function deepClone(obj) {
+  if (typeof obj !== 'object') return;
+  var newObj = obj instanceof Array ? [] : {};
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      newObj[key] = typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key];
+    }
+  }
+  return newObj;
+}
+
+let obj = {
+  name: 'Steven Lee',
+  props: {
+    static: true
+  }
+}
