098a56fa10c3516750e487fc88ac0c7376ad8219
StevenLee <qcjy16@126.com>
2021-04-11 23:35:17 +0800

    :typo: typo fix

--- nativeJs/shallowCopy&deepClone.js
+++ nativeJs/shallowCopy&deepClone.js
@@ -19,8 +19,8 @@ function shallowCopy(obj) {
 
 function deepClone(obj) {
   if (typeof obj !== 'object') return;
-  var newObj = obj instanceof Array ? [] : {};
-  for (var key in obj) {
+  const newObj = obj instanceof Array ? [] : {};
+  for (let key in obj) {
     if (obj.hasOwnProperty(key)) {
       newObj[key] = typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key];
     }
